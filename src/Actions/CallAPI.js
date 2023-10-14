9bf049f16d7910d0164cae11ace4a03275a16175
Marucha <marucha.webdev@yahoo.com>
2023-10-15 02:48:59 +0300

    feat: Action to fetch A Single Recipe

--- src/Actions/CallAPI.js
+++ src/Actions/CallAPI.js
@@ -3,8 +3,15 @@
 // API Call Hook
 import { useFetchData } from "../Hooks/FetchData"
 
+const apiKey = process.env.REACT_APP_SPOONACULAR_KEY
+
 export const GetRecipes = () => {
-  const apiKey = process.env.REACT_APP_SPOONACULAR_KEY
   const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`
+  return useFetchData(url)
+}
+
+
+export const GetRecipeById = (id) => {
+  const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
   return useFetchData(url)
 }
