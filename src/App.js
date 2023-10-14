0a885044dfa2b6e5858deeba2bd9fad0432c1911
Marucha <marucha.webdev@yahoo.com>
2023-10-14 23:12:33 +0300

    feat: Remove Strict Mode; useEffect re-renders call API multiply

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div
       className="text-small tablet:text-regular font-global min-h-screen min-w-screen
-       bg-gradient-to-b from-slate-50 via-slate-200 to-slate-300 text-zinc-800">
+       bg-gradient-to-tr from-slate-300 via-slate-100 to-slate-50 text-zinc-800">
       <Paths />
     </div>
   );
