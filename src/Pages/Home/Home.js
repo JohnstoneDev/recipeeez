cab4f27236ae5851be0139a88ca02bdc2560a696
Marucha <marucha.webdev@yahoo.com>
2023-10-15 02:49:26 +0300

    style: Styling for pages

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import { HomeComponent } from "../../Components/Home/HomeComponent"
 
 //  This should be a presentational Component
-export const Home = () => {
+export const HomePage = () => {
 	return (
 		<HomeComponent />
 	)
